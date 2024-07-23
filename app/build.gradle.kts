e684f09d48072f249ae5a8453f022496a553cd90
Bezes13 <AlexBelzer98@gmail.com>
2024-07-23 10:27:19 +0200

    new release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.games.colormix"
         minSdk = 24
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.1"
+        versionCode = 13
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
